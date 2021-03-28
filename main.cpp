140262cb8a7837ba83036b0d78df8990d4fc39a5
Kosta <68782786+KostaD02@users.noreply.github.com>
2021-03-28 16:23:07 +0400

    somChanges

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ using namespace std;
 #define Nov 30
 #define Dec 31
 
-class myDate{
+struct myDate{
 	private:
 		unsigned int day;
 		unsigned int month;
@@ -115,15 +115,84 @@ myDate nextDay(myDate currentDay){
 	if(Month==12 && Day>=Dec) return myDate(1,1,Year+1);
 	return myDate(Day+1,Month,Year);
 }
-
+struct myTime{
+	private:
+		unsigned int second;
+		unsigned int minute;
+		unsigned int hour;
+	public:
+	myTime(	unsigned int Second=1,unsigned int Minute=1,unsigned int Hour=1){
+		set(Second,Minute,Hour);
+	}
+	unsigned int getParams(unsigned int x=0){
+		unsigned int temp=1;
+		if(x==1) return second;
+		if(x==2) return minute;
+		if(x==3) return hour;
+		return temp;
+	} 
+	void set(unsigned int Second=1,unsigned int Minute=1,unsigned int Hour=1){
+		setSecond(Second);
+		setMinute(Minute);
+		setHour(Hour);
+	}
+	void setSecond(unsigned int Second){
+		if(Second<1) Second=0;
+		if(Second>59) Second=59;
+		second=Second;
+	}
+	void setMinute(unsigned int Minute){
+		if(Minute<1) Minute=0;
+		if(Minute>59) Minute=59;
+		minute=Minute;
+	}
+	void setHourDefault(unsigned int Hour){
+		if(Hour<1) Hour=0;
+		if(Hour>12) Hour=11;
+		hour=Hour;
+	}
+	void setHour(unsigned int Hour){
+		if(Hour<1) Hour=0;
+		if(Hour>24) Hour=23;
+		hour=Hour;
+	}
+//	unsigned int checkFormat(bool format,unsigned int Hour){
+//		if(format) return setHour(Hour);
+//		else return setHourDefault(Hour);
+//	}
+	string displayStr(char DEL=':'){
+		char buffer[4];
+		string temp="";
+		if(hour<10) temp=temp+"0"+itoa(hour,buffer,10);
+		else temp+=itoa(hour,buffer,10);
+		temp+=DEL;
+		if(minute<10) temp=temp+"0"+itoa(minute,buffer,10);
+		else temp+=itoa(minute,buffer,10);
+		temp+=DEL;
+		if(second<10) temp=temp+"0"+itoa(second,buffer,10);
+		else temp+=itoa(second,buffer,10);
+		return temp;
+	}
+};
+myTime nextHour(myTime currentTime){
+	unsigned int Seconds=currentTime.getParams(1);
+	unsigned int Minutes=currentTime.getParams(2);
+	unsigned int Hours=currentTime.getParams(3);
+}
 int main(int argc, char** argv) {
 	unsigned int currentDay=0;cout<<"Day=";cin>>currentDay;
 	unsigned int currentMonth=0;cout<<"Month=";cin>>currentMonth;
 	unsigned int currentYear=0;cout<<"Year=";cin>>currentYear;
 	myDate myDateStruct=myDate(currentDay,currentMonth,currentYear);
-	cout<<myDateStruct.displayStr()<<endl;
+	cout<<"DAY.MONTH.YEAR"<<endl<<myDateStruct.displayStr()<<endl;
 	myDate NextDay=nextDay(myDateStruct);
 	cout<<NextDay.displayStr()<<endl;
+	unsigned int currentHour=0;cout<<"Hour=";cin>>currentHour;
+	unsigned int currentMinute=0;cout<<"Minute=";cin>>currentMinute;
+	unsigned int currentSecond=0;cout<<"Second=";cin>>currentSecond;
+	myTime myTimeStruct=myTime(currentSecond,currentMinute,currentHour);
+	cout<<"HOURS:MINUTES:SECONDS"<<endl<<myTimeStruct.displayStr()<<endl;
+
 	//cout<<NextDay.day<<"."<<NextDay.month<<"."<<NextDay.year<<endl;
 	return 0;
 }
